8a36c48c8275bbac61bee6b90506aa8ed07c5c14
kdaniel1124 <kennellydanny@gmail.com>
2023-08-25 17:49:20 -0400

    DaysOfCode13
    Finished up the last two tests from Day12, then proceeded to write tests for and solve day 13.

---
+++ DaysOfCode/DaysOfCode.Test/Day13Tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode.Test
+{
+    [TestClass]
+    public class Day13Tests
+    {
+        [TestMethod]
+        public void DaysOfCode13_MakeEnds_NegativeSwap()
+        {
+            //Arrange
+            DaysOfCode13 Test1 = new DaysOfCode13();
+            int[] input = new int[] { -1, 0, 1 };
+            int[] expected = new int[] { -1, 1 };
+
+            //Act
+            int[] result = Test1.MakeEnds(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, result, "Method should return an array with swapped the beginning and end indexes and drop the numbers in between");
+        }
+
+        [TestMethod]
+        public void DaysOfCode13_MakeEnds_ArrayLengthOne()
+        {
+            //Arrange
+            DaysOfCode13 Test2 = new DaysOfCode13();
+            int[] input = new int[] { 42 };
+            int[] expected = new int[] { 42, 42 };
+
+            //Act
+            int[] result = Test2.MakeEnds(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, result, "If array has a length of one then it should return the number twice in an array");
+        }
+
+        [TestMethod]
+        public void DaysOfCode13_MakeEnds_LongArrayRandomOrder()
+        {
+            //Arrange
+            DaysOfCode13 Test3 = new DaysOfCode13();
+            int[] input = new int[] { 65, 3, -5, 12, -900, -27, 27, -64, 16 };
+            int[] expected = new int[] { 65, 16 };
+
+            //Act
+            int[] result = Test3.MakeEnds(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, result, "Should return an array of length two regardless of original array length or contents");
+        }
+
+    }
+}
---
+++ DaysOfCode/DaysOfCode/DaysOfCode13.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode13
+    {
+        public int[] MakeEnds(int[] nums)
+        {
+            int[] result = new int[] { nums[0], nums[nums.Length-1] };
+
+            return result;
+        }
+    }
+}
