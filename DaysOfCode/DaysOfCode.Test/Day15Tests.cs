ae9af8569b0777682028135e84ec913c7f37b06a
kdaniel1124 <kennellydanny@gmail.com>
2023-08-27 11:48:34 -0400

    Has23 Method
    Wrote tests for, then solved day 15's kata: Has23.

---
+++ DaysOfCode/DaysOfCode.Test/Day15Tests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode.Test
+{
+    [TestClass]
+    public class Day15Tests
+    {
+        [TestMethod]
+        public void DaysOfCode15_Has23_Pure23Test()
+        {
+            //Arrange
+            DaysOfCode15 Test1 = new DaysOfCode15();
+            int[] input = new int[] { 23, 23};
+
+            //Act
+            bool result = Test1.Has23(input);
+
+            //Assert
+            Assert.AreEqual(false, result, "method should (ironically) return false when given 23's because we are only for looking for 2's OR 3's");
+        }
+
+        [TestMethod]
+        public void DaysOfCode15_Has23_Both3And2()
+        {
+            //Arrange
+            DaysOfCode15 Test2 = new DaysOfCode15();
+            int[] input = new int[] { 3, 2};
+
+            //Act
+            bool result = Test2.Has23(input);
+
+            //Assert
+            Assert.AreEqual(true, result, "method should return true if 3 and/or 2 are present in the array");
+        }
+
+        [TestMethod]
+        public void DaysOfCode15_Has23_No2Or3()
+        {
+            //Arrange
+            DaysOfCode15 Test3 = new DaysOfCode15();
+            int[] input = new int[] { 0, 146};
+
+            //Act
+            bool result = Test3.Has23(input);
+
+            //Assert
+            Assert.AreEqual(false, result, "method should return false if no 2's or 3's are present");
+        }
+
+    }
+}
---
+++ DaysOfCode/DaysOfCode/DaysOfCode15.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode15
+    {
+        public bool Has23(int[] nums)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 2 || nums[i] == 3)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
