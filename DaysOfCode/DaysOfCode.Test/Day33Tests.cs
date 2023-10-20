b4eaaedfc1e7c4e020a7ad5cad0cef57d37ff895
kdaniel1124 <kennellydanny@gmail.com>
2023-10-19 20:08:39 -0400

    Day 33 solution and tests completed

---
+++ DaysOfCode/DaysOfCode.Test/Day33Tests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode.Test
+{
+    [TestClass]
+    public class Day33Tests
+    {
+        [TestMethod]
+        public void DaysOfCode33_ComboString_EmptyA()
+        {
+            //Arrange
+            DaysOfCode33 Test1 = new DaysOfCode33();
+            string inputA = "";
+            string inputB = "Camp";
+            string expected = "Camp";
+
+            //Act
+            string result = Test1.ComboString(inputA, inputB);
+
+            //Assert
+            Assert.AreEqual(expected, result, "Method should determine which string is shorter, then return the strings combined in this order: 'shorter + longer + shorter");
+        }
+
+        [TestMethod]
+        public void DaysOfCode33_ComboString_SameLength()
+        {
+            //Arrange
+            DaysOfCode33 Test2 = new DaysOfCode33();
+            string inputA = "Bake";
+            string inputB = "Take";
+            string expected = "Invalid input, cannot compute. Must enter two strings of different length.";
+
+            //Act
+            string result = Test2.ComboString(inputA, inputB);
+
+            //Assert
+            Assert.AreEqual(expected, result, "Method should determine which string is shorter, if they are same length then return a message asking for two strings of different lengths");
+        }
+
+        [TestMethod]
+        public void DaysOfCode33_ComboString_LongA()
+        {
+            //Arrange
+            DaysOfCode33 Test3 = new DaysOfCode33();
+            string inputA = "Banana";
+            string inputB = "Cake";
+            string expected = "CakeBananaCake";
+
+            //Act
+            string result = Test3.ComboString(inputA, inputB);
+
+            //Assert
+            Assert.AreEqual(expected, result, "Method should determine which string is shorter, then return the strings combined in this order: 'shorter + longer + shorter");
+        }
+
+    }
+}
---
+++ DaysOfCode/DaysOfCode/DaysOfCode33.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode33
+    {
+        public string ComboString(string a, string b)
+        {
+            //Find out which string is longer (they will never be the same length, although one may be empty)
+            //Whichever string is shorter will be the bread in the string sandwich, while the longer string will be the meat
+            //ie: a='for' b='banana' then the method should return 'forbananafor'
+
+            string result = "";
+
+            if(a.Length > b.Length)
+            {
+                result = b + a + b;
+            }
+            else if(a.Length < b.Length)
+            {
+                result = a + b + a;
+            }
+            else
+            {
+                result = "Invalid input, cannot compute. Must enter two strings of different length.";
+            }
+            return result;
+        }
+    }
+}
