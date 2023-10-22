13ded3685be948957a5c91180337806b08e6599f
kdaniel1124 <kennellydanny@gmail.com>
2023-10-22 11:17:54 -0400

    Day 34 Solution and one test, need to go back and write two more

---
+++ DaysOfCode/DaysOfCode.Test/Day34Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode.Test
+{
+    [TestClass]
+    public class Day34Tests
+    {
+        [TestMethod]
+        public void DaysOfCode34_HelloName_LowerCaseDaniel()
+        {
+            //Arrange
+            DaysOfCode34 Test1 = new DaysOfCode34();
+            string input = "daniel";
+            string expected = "Hello Daniel!";
+
+            //Act
+            string result = Test1.HelloName(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "Method should capitalize the first letter of user's name and return it in the format of 'Hello {name}!'");
+        }
+    }
+}
---
+++ DaysOfCode/DaysOfCode/DaysOfCode34.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode34
+    {
+        public string HelloName(string name)
+        {
+            string upperName = name[0].ToString().ToUpper() + name.Substring(1);
+            name[0].ToString().ToUpper();
+
+            return ($"Hello {upperName}!");
+        }
+    }
+}
