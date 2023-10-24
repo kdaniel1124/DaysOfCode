926613c7e1a8b38ed5af501d2141d98e0dd2c0c1
kdaniel1124 <kennellydanny@gmail.com>
2023-10-24 14:18:37 -0400

    Day 35 solution and tests completed

---
+++ DaysOfCode/DaysOfCode.Test/Day35Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode.Test
+{
+    [TestClass]
+    public class Day35Tests
+    {
+        [TestMethod]
+        public void DaysOfCode35_HasBad_()
+        {
+            //Arrange
+            DaysOfCode35 Test1 = new DaysOfCode35();
+            string input = "xbadx";
+
+            //Act
+            bool result = Test1.HasBad(input);
+
+            //Assert
+            Assert.AreEqual(true, result, "If the string contains the word 'bad' within the first five chars of the string then we should return true.");
+
+        }
+    }
+}
---
+++ DaysOfCode/DaysOfCode/DaysOfCode35.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode35
+    {
+        public bool HasBad(string str)
+        {
+            return (str.Contains("bad"));
+        }
+    }
+}
