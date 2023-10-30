479299d3946ce16b7c13fa35e1e985e65d4e9d65
kdaniel1124 <kennellydanny@gmail.com>
2023-10-30 19:41:50 -0400

    CodeWars JadenCase Solution And Tests Completed

--- DaysOfCode/DaysOfCode.Test/CodeWars_JadenCasedTests.cs
+++ DaysOfCode/DaysOfCode.Test/CodeWars_JadenCasedTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,55 @@ using System.Threading.Tasks;
 
 namespace DaysOfCode.Test
 {
-    internal class CodeWars_JadenCasedTests
+    [TestClass]
+    public class CodeWars_JadenCasedTests
     {
+        [TestMethod]
+        public void CodeWars_JadenCased_ToJadenCase_EmptyString()
+        {
+            //Arrange
+            CodeWars_JadenCased Test1 = new CodeWars_JadenCased();
+            string input = "";
+            string expected = "";
+
+            //Act
+            string result = Test1.ToJadenCase(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "If provided an empty string then no changes need to be made and the method should simply return an empty string");
+
+        }
+
+        [TestMethod]
+        public void CodeWars_JadenCased_ToJadenCase_LongStringToBeJadened()
+        {
+            //Arrange
+            CodeWars_JadenCased Test2 = new CodeWars_JadenCased();
+            string input = "oh yes my name is jaden indeed";
+            string expected = "Oh Yes My Name Is Jaden Indeed";
+
+            //Act
+            string result = Test2.ToJadenCase(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "When provided a string method should return the string with each word capitalized");
+
+        }
+
+        [TestMethod]
+        public void CodeWars_JadenCased_ToJadenCase_AlreadyJadenCase()
+        {
+            //Arrange
+            CodeWars_JadenCased Test3 = new CodeWars_JadenCased();
+            string input = "Jaden Was Here";
+            string expected = "Jaden Was Here";
+
+            //Act
+            string result = Test3.ToJadenCase(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "Method should return the same string but with the first letter of each word capitalized. If they are already capitalized, then the word should remain unchanged");
+
+        }
     }
 }
--- DaysOfCode/DaysOfCode/CodeWars_JadenCased.cs
+++ DaysOfCode/DaysOfCode/CodeWars_JadenCased.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace DaysOfCode
 {
-    internal class CodeWars_JadenCased
+    public class CodeWars_JadenCased
     {
+        public string ToJadenCase(string phrase)
+        {
+            string[] jadeBank = phrase.Split(" ");
+            string result = "";
+            if (phrase.Length > 0)
+            {
+                foreach (string word in jadeBank)
+                {
+                    result += (char.ToUpper(word[0]) + word.Substring(1) + " ");
+                }
+            }
+
+            return result.Trim();
+            /*string result = "";
+
+            if (phrase.Length > 0)
+            {
+                result += phrase[0].ToString().ToUpper();
+
+                for (int i = 1; i < phrase.Length; i++)
+                {
+                    if (phrase[i - 1].ToString() == " ")
+                    {
+                        result += phrase[i].ToString().ToUpper();
+                    }
+                    else
+                    {
+                        result += phrase[i];
+                    }
+                }
+            }
+
+            return result;*/
+        }
     }
 }
