9fda707f783c47d5a39f32f2085a5efa0969e4af
kdaniel1124 <kennellydanny@gmail.com>
2023-11-03 12:37:30 -0400

    Day 39 solution completed and tests passing

---
+++ DaysOfCode/DaysOfCode.Test/Day39Tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode.Test
+{
+    [TestClass]
+    public class Day39Tests
+    {
+        [TestMethod]
+        public void DaysOfCode39_ExtraEnd_LongWordTest()
+        {
+            //Arrange
+            DaysOfCode39 Test1 = new DaysOfCode39();
+            string input = "September is a swell month!";
+            string expected = "h!h!h!";
+
+            //Act
+            string result = Test1.ExtraEnd(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "Method should grab the last 2 characters of the string and return it three times in a row");
+        }
+
+        [TestMethod]
+        public void DaysOfCode39_ExtraEnd_EmptyString()
+        {
+            //Arrange
+            DaysOfCode39 Test2 = new DaysOfCode39();
+            string input = "";
+            string expected = "";
+
+            //Act
+            string result = Test2.ExtraEnd(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "If provided an empty string then method should simply return an empty string");
+        }
+
+        [TestMethod]
+        public void DaysOfCode39_ExtraEnd_StringLengthOne()
+        {
+            //Arrange
+            DaysOfCode39 Test3 = new DaysOfCode39();
+            string input = "3";
+            string expected = "333";
+
+            //Act
+            string result = Test3.ExtraEnd(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "If string is only length 1 then simply return what is there three times");
+        }
+
+    }
+}
---
+++ DaysOfCode/DaysOfCode/DaysOfCode39.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode39
+    {
+        public string ExtraEnd(string str)
+        {
+            string last = "";
+            if (str.Length > 1)
+            {
+                last = str.Substring(str.Length - 2);
+            }
+            else if (str.Length == 1)
+            {
+                last = str.Substring(str.Length - 1);
+            }
+
+            return (last + last + last);
+        }
+    }
+}
