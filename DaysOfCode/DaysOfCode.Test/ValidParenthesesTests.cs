4d729c38e35e310c83667db6c722cfc97fa4c80f
kdaniel1124 <kennellydanny@gmail.com>
2023-11-06 12:10:05 -0500

    validparentheses method and tests completed

---
+++ DaysOfCode/DaysOfCode.Test/ValidParenthesesTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode.Test
+{
+    [TestClass]
+    public class ValidParenthesesTests
+    {
+        [TestMethod]
+        public void ValidParenthesesKata_ValidParentheses_CloseAtStart()
+        {
+            //Arrange
+            ValidParenthesesKata Test1 = new ValidParenthesesKata();
+            string input = ")(())(";
+            bool expected = false;
+
+            //Act
+            bool result = Test1.ValidParentheses(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "At no point should there be more close parentheses than open");
+        }
+
+    }
+}
---
+++ DaysOfCode/DaysOfCode/ValidParenthesesKata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class ValidParenthesesKata
+    {
+        public bool ValidParentheses(string str)
+        {
+            int open = 0;
+            int close = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (str[i] == '(')
+                {
+                    open++;
+                }
+                if (str[i] == ')')
+                {
+                    close++;
+                }
+                if (close > open)
+                {
+                    return false;
+                }
+            }
+            return (open == close);
+        }
+    }
+}
