17f9807f1f6211d11a206a4b541a3c60511d59c0
kdaniel1124 <kennellydanny@gmail.com>
2023-11-16 15:53:16 -0500

    day 52 solution complete. Tests TODO.

---
+++ DaysOfCode/DaysOfCode/DaysOfCode52.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode52
+    {
+        public string MakeOutWord(string outWord, string inWord)
+        {
+            if(outWord.Length < 1)
+            {
+                return inWord;
+            }
+            else
+            {
+                char outLeft = outWord[0];
+                char outRight = outWord[outWord.Length - 1];
+
+                return (outLeft + outLeft + inWord + outRight + outRight);
+            }
+        }
+    }
+}
