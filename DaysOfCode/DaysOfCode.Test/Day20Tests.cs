9df527729c31b345a10ac01a5e78c9efd7d6e61a
kdaniel1124 <kennellydanny@gmail.com>
2023-12-07 17:39:17 -0500

    Day 20 classes created, TDD approach.

---
+++ DaysOfCode/DaysOfCode.Test/Day20Tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode.Test
+{
+    [TestClass]
+    public class Day20Tests
+    {
+        [TestMethod]
+        public void DaysOfCode20_BiggerTwo_FirstBigger()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [TestMethod]
+        public void DaysOfCode20_BiggerTwo_Negatives()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [TestMethod]
+        public void DaysOfCode20_BiggerTwo_Tie()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+    }
+}
---
+++ DaysOfCode/DaysOfCode/DaysOfCode20.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode20
+    {
+        // Create an integer array method called BiggerTwo that takes in two integer arrays “a” and “b”. Start with 2 int arrays, a and b, each length 2.
+        // Consider the sum of the values in each array. Return the array which has the largest sum. 
+        // In event of a tie, return a.
+
+        
+    }
+}
