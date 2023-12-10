4d4ee478a90b9432468f7f171a31d53e213d036a
kdaniel1124 <kennellydanny@gmail.com>
2023-12-10 11:09:50 -0500

    Day 9 Solution completed. TODO: Unit tests

---
+++ DaysOfCode/DaysOfCode/DaysOfCode9.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode9
+    {
+        public bool No14(int[] arr)
+        {
+            return (arr.Contains(1) || arr.Contains(4));
+        }
+    }
+}
