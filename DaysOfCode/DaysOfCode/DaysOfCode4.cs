f062646dbeaf48c2b69dc13b3390f8219d778770
kdaniel1124 <kennellydanny@gmail.com>
2024-01-02 19:18:21 -0500

    Days of Code exercise 4 solution completed. TODO: write unit tests

---
+++ DaysOfCode/DaysOfCode/DaysOfCode4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode4
+    {
+        public int Start1(int[] a, int[] b)
+        {
+            int result = 0;
+
+            if (a[0] == 1 && b[0] == 1)
+            {
+                return 2;
+            }
+            else if (a[0] != 1 && b[0] != 1)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
