8a296f438592bce53dd0f01aa4e70e435ba57391
kdaniel1124 <kennellydanny@gmail.com>
2024-01-04 14:58:46 -0500

    Days of code exercise 21 solution complete. TODO: write unit tests

---
+++ DaysOfCode/DaysOfCode/DaysOfCode21.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfCode
+{
+    public class DaysOfCode21
+    {
+        public int Blackjack(int a, int b)
+        {
+            if(a >= b && a <= 21)
+            {
+                return a;
+            }
+            else if (b > a && b <= 21)
+            {
+                return b;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
--- DaysOfCode/DaysOfCode/obj/Debug/net6.0/DaysOfCode.AssemblyInfo.cs
+++ DaysOfCode/DaysOfCode/obj/Debug/net6.0/DaysOfCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DaysOfCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86ae9e1ef362acc2a22d4387f86d5dc9e433043")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DaysOfCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DaysOfCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
