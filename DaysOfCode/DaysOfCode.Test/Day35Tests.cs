04aade217cbd96a8201b0dacd44739fa90e2de5d
kdaniel1124 <kennellydanny@gmail.com>
2024-01-12 20:32:57 -0500

    Finished writing tests for Days of code 35

--- DaysOfCode/DaysOfCode.Test/Day35Tests.cs
+++ DaysOfCode/DaysOfCode.Test/Day35Tests.cs
@@ -11,7 +11,7 @@ namespace DaysOfCode.Test
     public class Day35Tests
     {
         [TestMethod]
-        public void DaysOfCode35_HasBad_()
+        public void DaysOfCode35_HasBad_MiddleBad()
         {
             //Arrange
             DaysOfCode35 Test1 = new DaysOfCode35();
@@ -22,7 +22,35 @@ namespace DaysOfCode.Test
 
             //Assert
             Assert.AreEqual(true, result, "If the string contains the word 'bad' within the first five chars of the string then we should return true.");
-
         }
+
+        [TestMethod]
+        public void DaysOfCode35_HasBad_JustBad()
+        {
+            //Arrange
+            DaysOfCode35 Test2 = new DaysOfCode35();
+            string input = "bad";
+
+            //Act
+            bool result = Test2.HasBad(input);
+
+            //Assert
+            Assert.AreEqual(true, result, "If the string contains the word 'bad' within the first five chars of the string then we should return true.");
+        }
+
+        [TestMethod]
+        public void DaysOfCode35_HasBad_NoBad()
+        {
+            //Arrange
+            DaysOfCode35 Test3 = new DaysOfCode35();
+            string input = "xdabx";
+
+            //Act
+            bool result = Test3.HasBad(input);
+
+            //Assert
+            Assert.AreEqual(false, result, "If the string contains the word 'bad' within the first five chars of the string then we should return true.");
+        }
+
     }
 }
